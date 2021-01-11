9af26763a51390a87d9ab0e841c94104899a7ac8
terahje <terahjequinn@gmail.com>
2021-01-10 17:48:08 -0700

    started refactor, added store.js and updated reducers

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Success from "./pages/Success";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+
+// replaced {storeProvider } with following:
+import { Provider } from 'react-redux';
+// Import the Redux store you created
+import store from './utils/store';
+
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -30,7 +35,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -41,7 +46,7 @@ function App() {
               <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,5 +1,3 @@
-import { useReducer } from 'react';
-
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
@@ -12,7 +10,16 @@ import {
     TOGGLE_CART
   } from './actions';
 
-export const reducer = (state, action) => {
+  const initialState = {
+    products: [],
+    categories: [],
+    currentCategory: '',
+    cart: [],
+    cartOpen: false
+    
+  };
+
+export const reducers = (state= initialState, action) => {
     switch (action.type) {
         // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
         case UPDATE_PRODUCTS:
@@ -85,7 +92,5 @@ export const reducer = (state, action) => {
             return state;
     }
 };
-
-export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
-  }
+// Export the reducer
+export default reducers;
